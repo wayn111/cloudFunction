deb3e0a8341e394e4010348f16a6b9e57cc62a24
waynaqua <166974380@qq.com>
2021-07-29 20:33:18 +0800

    feat(油价推送): 推送消息优化

--- send-msg-gasoline/gasoline.php
+++ send-msg-gasoline/gasoline.php
@@ -24,6 +24,10 @@ function getGasolineInfo($event, $context)
     $price = $html->find('#youjia dd')->text();
     // 获取提示信息
     $tip = $ql->find('div:nth-child(2)')->text();
+    $tipArr = explode("\r\n", $tip);
+    $dateTip = $tipArr[0];
+    $priceTip = $tipArr[1];
+    $tip = sprintf('%s,%s', $dateTip, $priceTip);
     $qiyouArr = explode("\n", $qiyou);
     $priceArr = explode("\n", $price);
     $_92 = $priceArr[0];
@@ -31,10 +35,10 @@ function getGasolineInfo($event, $context)
 
     $text['url'] = 'http://www.qiyoujiage.com/hubei/xianning.shtml';
     $text['title'] = <<<EOF
-咸宁今日油价 单位:元/升
-92#汽油：{$_92} | 95#汽油：{$_95}
+咸宁油价提醒 92#汽油:{$_92} 95#汽油:{$_95}
+{$dateTip}
 EOF;
-    $text['description'] = '';
+    $text['description'] = "单位:元/升\n";
 
     foreach ($qiyouArr as $key => $item) {
         $text['description'] .= sprintf("%s:%s\n", $item, $priceArr[$key]);
