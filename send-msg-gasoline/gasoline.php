519c0bfd7dd4e997a707469e2e08a49c4734b4de
waynaqua <1669738430@qq.com>
2022-04-05 13:44:16 +0800

    读取环境变量

--- send-msg-gasoline/gasoline.php
+++ send-msg-gasoline/gasoline.php
@@ -15,39 +15,45 @@ getGasolineInfo(null, null);
 
 function getGasolineInfo($event, $context)
 {
+    $str = getenv('spiderUrl') ?: 'http://www.qiyoujiage.com/hubei/xianning.shtml,http://www.qiyoujiage.com/hubei/wuhan.shtml';
+    $spiderUrlArr  = explode(",", $str);
     $config = $GLOBALS['appConfig'];
-    $ql = QueryList::get('http://www.qiyoujiage.com/hubei/xianning.shtml')
-        ->find('#youjiaCont');
-    $html = $ql->html();
-    // 乱码处理
-    $html = QueryList::html($html)->encoding("UTF-8", "UTF-8");
-    // 获取a元素对象
-    $qiyou = $html->find('#youjia dt')->text();
-    $price = $html->find('#youjia dd')->text();
-    // 获取提示信息
-    $tip = $ql->find('div:nth-child(2)')->html();
-    $tipArr = explode("<br>", $tip);
-    $dateTip = $tipArr[0];
-    $priceTip = QueryList::html($tipArr[1])->find('span:nth-child(1)')->text();
-    $tip = sprintf('%s,%s', $dateTip, $priceTip);
-    $qiyouArr = explode("\n", $qiyou);
-    $priceArr = explode("\n", $price);
-    $_92 = $priceArr[0];
-    $_95 = $priceArr[1];
+    foreach ($spiderUrlArr as $base_url) {
+        $ql = QueryList::get($base_url)
+            ->find('#youjiaCont');
+        $html = $ql->html();
+        // 乱码处理
+        $html = QueryList::html($html)->encoding("UTF-8", "UTF-8");
+        // 获取a元素对象
+        $qiyou = $html->find('#youjia dt')->text();
+        $price = $html->find('#youjia dd')->text();
+        // 获取提示信息
+        $tip = $ql->find('div:nth-child(2)')->html();
+        $tipArr = explode("<br>", $tip);
+        $dateTip = $tipArr[0];
+        $priceTip = QueryList::html($tipArr[1])->find('span:nth-child(1)')->text();
+        $tip = sprintf('%s,%s', $dateTip, $priceTip);
+        $qiyouArr = explode("\n", $qiyou);
+        $priceArr = explode("\n", $price);
+        $_92 = $priceArr[0];
+        $_95 = $priceArr[1];
 
-    $text['url'] = 'http://www.qiyoujiage.com/hubei/xianning.shtml';
-    $text['title'] = <<<EOF
+        $text['url'] = $base_url;
+        $text['title'] = <<<EOF
 咸宁油价提醒 $dateTip
 92汽油:$_92 95汽油:$_95
 EOF;
-    $text['description'] = "单位:元/升\n";
+        $text['description'] = "单位:元/升\n";
 
-    foreach ($qiyouArr as $key => $item) {
-        $text['description'] .= sprintf("%s:%s\n", $item, $priceArr[$key]);
+        foreach ($qiyouArr as $key => $item) {
+            $text['description'] .= sprintf("%s:%s\n", $item, $priceArr[$key]);
+        }
+        if ($tip) {
+            $text['description'] .= "\n提示：$tip";
+        }
+        print_r($text);
+        WecomSendClass::sendMsg($text, $config['wecom_cid'], $config['wecom_aid'], $config['wecom_secret']);
     }
-    if ($tip) {
-        $text['description'] .= "\n提示：$tip";
-    }
-    WecomSendClass::sendMsg($text, $config['wecom_cid'], $config['wecom_aid'], $config['wecom_secret']);
+
 
 }
